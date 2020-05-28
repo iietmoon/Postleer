cce52bb91ee54c824400ad58d1d4416ae1a324d3
Othmane N <59323287+iietmoon@users.noreply.github.com>
2020-05-28 22:36:00 +0100

    onDev

---
+++ inc/tabs/about.php
@@ -0,0 +1,51 @@
+<div class="asap-section" id="asap-section-about" <?php if ($active_tab != 'about') { ?>style="display: none;"<?php } ?>>
+    <div class="about-pinterest-wrapper clearfix">
+        <div class="about-desc-wrap clearfix">
+            <div class="more-title"> About FAuto Poster</div>
+            <div class="about-content">
+                <p><strong>FAuto Poster</strong> is a feature rich free plugin for WordPress to auto post your blog content to your facebook fan page/profile page. Within few minutes setup you can publish your WordPress post automatically to your Facebook profile and fan pages.</p>
+            </div>
+                 <h3>Themes Compatible with the Plugin :</h3>
+
+                <strong>FAuto Poster</strong> works best with every WordPress theme. It's even more remarkable when used with popular themes like VMagazine and AccessPress Parallax.
+                <p><strong>AND IF THIS PLUGIN HAS IMPRESSED YOU, THEN YOU WOULD ENJOY OUR OTHER PROJECTS TOO. DO CHECK THESE OUT :</strong></p>
+               <ul>
+                <li>
+                <a href="https://wpall.club/">WPAll Club</a> -  A complete WordPress resources club. WordPress tutorials, blogs, curated free and premium themes and plugins, WordPress deals, offers, hosting info and more.</li>
+
+                <li><a href="https://themeforest.net/user/accesskeys/portfolio">Premium WordPress Themes</a> -   6 premium WordPress themes well suited for all sort of websites. Professional, well coded and highly configurable themes for you. </li>
+
+                <li><a href="https://codecanyon.net/user/accesskeys/portfolio?Ref=AccessKeys">Premium WordPress Plugins</a> - 45+ premium WordPress plugins of many different types. High user ratings, great quality and best sellers in CodeCanyon marketplace.</li>
+
+                <li><a href="https://accesspressthemes.com/">AccessPress Themes</a> - AccessPress Themes has 50+ beautiful and elegant, fully responsive, multipurpose themes to meet your need for free and commercial basis.</li>
+
+                <li><a href="https://8degreethemes.com/">8Degree Themes</a> - 8Degree Themes offers 15+ free WordPress themes and 16+ premium WordPress themes carefully crafted with creativity.</li>
+               </ul>
+        </div>
+    </div>
+    <div class="social-more-wrap clearfix">
+        <div class="more-title"> More from AccessPress Themes </div>
+        <ul class="more-product">
+            <li><a href="https://accesspressthemes.com/plugins/" target="_blank"><span class="prod-title"> Wordpress Plugins </span> <img src="<?php echo AFAP_IMG_DIR . '/plugin.png'; ?>" width="100%" /> </a></li>
+            <li><a href="https://accesspressthemes.com/themes/" target="_blank"><span class="prod-title"> Wordpress Themes </span> <img src="<?php echo AFAP_IMG_DIR . '/theme.png'; ?>" width="100%" /></a></li>
+            <li><a href="https://accesspressthemes.com/contact/" target="_blank"><span class="prod-title"> Wordpress Customization </span> <img src="<?php echo AFAP_IMG_DIR . '/customize.png'; ?>" width="100%" /></a></li>
+        </ul>
+    </div>
+
+    <div class="social-share-wrap clearfix">
+        <div class="more-title"> Get social </div>
+        <div class="social-iframe"> <strong>Like us on facebook:</strong><br />
+            <iframe style="border: none; overflow: hidden; width: 764px; height: 206px;" src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FAccessPress-Themes%2F1396595907277967&width=842&height=258&colorscheme=light&show_faces=true&header=false&stream=false&show_border=true&appId=1411139805828592" width="240" height="150" frameborder="0" scrolling="no"></iframe></div>
+        <ul class="about-social clearfix">
+            <li><a href="https://www.facebook.com/pages/AccessPress-Themes/1396595907277967" class="fb" target="_blank"><i class="fa fa-facebook"> </i> <span> Follow us on <span class="bold"> Facebook </span></span></a></li>
+            <li><a href="https://twitter.com/apthemes" class="twt" target="_blank"><i class="fa fa-twitter"> </i><span> Follow us on <span class="bold"> Twitter </span> </span></a></li>
+            <li><a href="https://plus.google.com/+Accesspressthemesprofile/" class="gp" target="_blank"><i class="fa fa-google-plus"> </i><span> Follow us on <span class="bold"> Google + </span> </span></a></li>
+            <li><a href="https://www.youtube.com/user/accesspressthemes" class="utube" target="_blank"><i class="fa fa-youtube"> </i><span> Subscribe us on <span class="bold"> Youtube </span> </span></a></li>
+            <li><a href="skype:access-keys" class="skype" target="_blank"><i class="fa fa-skype"> </i><span> Contact us on <span class="bold"> Skype </span> </span></a></li>
+            <li><a href="https://www.pinterest.com/accesspresswp/" class="pin" target="_blank"><i class="fa fa-pinterest"> </i><span> Follow us on <span class="bold"> Pinterest </span> </span></a></li>
+        </ul>
+    </div>
+
+
+
+</div>
